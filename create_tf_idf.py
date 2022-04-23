42f7db5344ddbb55c5327ee3096c1f45277f403e
Bhushan Borole <borolebhushan8@gmail.com>
2022-04-23 16:31:32 +0100

    outputs generating

--- create_tf_idf.py
+++ create_tf_idf.py
@@ -9,6 +9,7 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.stem import WordNetLemmatizer
 import itertools
+from scipy.stats import mannwhitneyu
 
 ROOT = "./Database"
 
@@ -17,9 +18,10 @@ DOCUMENTS_ABC = {}
 DATASET = {}
 
 LYRICS = "Lyrics/"
-ABC = "ABC/"
+ABC = "ABC_processed/"
 
 AGGREGATE_ARTIST = {}
+AGGREGATE_PAIR_ARTISTS = {}
 
 
 def populate_dataset_dict():
@@ -30,7 +32,7 @@ def populate_dataset_dict():
             for nested_dir in os.listdir(ROOT + "/" + directory + "/Lyrics/"):
                 if os.path.isdir(nested_path + nested_dir):
                     DATASET[directory][nested_dir] = [
-                        x.replace(".txt", "")
+                        x.replace(".txt", "").replace(" ", "").lower()
                         for x in os.listdir(nested_path + nested_dir)
                     ]
 
@@ -61,7 +63,7 @@ def read_file(path):
 def populate_documents(root, files, dir_name):
     for file in files:
         lines = read_file(root + "/" + file)
-        file = file.replace(".txt", "").replace(".abc", "")
+        file = file.replace(".txt", "").replace(".abc", "").lower().replace(" ", "")
         if dir_name == LYRICS:
             lines = lines.translate(str.maketrans("", "", string.punctuation))
             lines = lines.translate(str.maketrans("", "", string.digits))
@@ -77,6 +79,7 @@ def populate_documents(root, files, dir_name):
 
 def get_cosine_sim_mat(documents, stopwords):
     # Generates TF-IDF values and then a cosine similarity matrix
+    print("Documents len", len(documents.values()))
     tfidf, tfidf_vector = gen_tf_idf(documents.values(), stopwords)
     df = pd.DataFrame(
         tfidf_vector.toarray(),
@@ -110,8 +113,36 @@ def generate_pairs_of_artist():
     li = []
     for genre in DATASET:
         li.extend(DATASET[genre].keys())
-    combinations = itertools.combinations(li, 2)
-    return [x for x in combinations]
+    print(len(li))
+    combinations = list(itertools.permutations(li, 2))
+    # combinations = [(a, b) for idx, a in enumerate(li) for b in li[idx + 1 :]]
+    print("Length of combinations: ", len(combinations))
+    return combinations
+
+
+def get_songs_from_artist(artist):
+    for genre in DATASET:
+        for a in DATASET[genre]:
+            if a == artist:
+                return DATASET[genre][a]
+    return None
+
+
+def get_genre_from_artist(artist):
+    for genre in DATASET:
+        for a in DATASET[genre]:
+            if a == artist:
+                return genre
+    return None
+
+
+def get_absolute_differences_mean(mat1, mat2):
+    return abs(mat1 - mat2).mean()
+
+
+def mann_whitney_rank(mat1, mat2):
+    U1, p = mannwhitneyu(mat1.flatten(), mat2.flatten())
+    return p
 
 
 def main():
@@ -123,15 +154,17 @@ def main():
         elif ABC in root:
             populate_documents(root, files, ABC)
 
-    df_lyrics, cosine_sim_lyrics, cosine_sim_lyrics_df = get_cosine_sim_mat(
-        DOCUMENTS_LYRICS, stopwords="english"
-    )
-    # df_abc, cosine_sim_abc, cosine_sim_abc_df = get_cosine_sim_mat(
-    #     DOCUMENTS_ABC, stopwords=None
-    # )
+    # pprint(DATASET)
 
-    sanity_check(df_lyrics, cosine_sim_lyrics_df, "BookOfDays", "Blueberry Hill")
-    # sanity_check(df_abc, cosine_sim_abc_df, "BookOfDays", "What A Wonderful World")
+    df_lyrics, cosine_sim_lyrics, cosine_sim_lyrics_df = get_cosine_sim_mat(
+        DOCUMENTS_LYRICS, stopwords=None
+    )
+    df_abc, cosine_sim_abc, cosine_sim_abc_df = get_cosine_sim_mat(
+        DOCUMENTS_ABC, stopwords=None
+    )
+
+    # sanity_check(df_lyrics, cosine_sim_lyrics_df, "BookOfDays", "BlueberryHill")
+    # sanity_check(df_abc, cosine_sim_abc_df, "BookOfDays", "WhatAWonderfulWorld")
 
     for genre in DATASET:
         for artist in DATASET[genre]:
@@ -140,16 +173,45 @@ def main():
                 DATASET[genre][artist]
             ]
             temp_lyrics_df_mat = temp_lyrics_df.to_numpy()
-            AGGREGATE_ARTIST[artist]["Lyrics"] = np.linalg.det(temp_lyrics_df_mat)
+            # AGGREGATE_ARTIST[artist]["Lyrics"] = np.linalg.det(temp_lyrics_df_mat)
+            # AGGREGATE_ARTIST[artist]["Lyrics"] = temp_lyrics_df_mat.mean()
 
             # Uncomment when ABC files are present
-            # temp_abc_df = cosine_sim_abc_df.loc[DATASET[genre][artist]][
-            #     DATASET[genre][artist]
-            # ]
-            # temp_abc_df_mat = temp_abc_df.to_numpy()
+            temp_abc_df = cosine_sim_abc_df.loc[DATASET[genre][artist]][
+                DATASET[genre][artist]
+            ]
+            temp_abc_df_mat = temp_abc_df.to_numpy()
             # AGGREGATE_ARTIST[artist]["ABC"] = np.linalg.det(temp_abc_df_mat)
+            # AGGREGATE_ARTIST[artist]["ABC"] = temp_abc_df_mat.mean()
+
+            AGGREGATE_ARTIST[artist] = round(
+                1 - get_absolute_differences_mean(temp_lyrics_df_mat, temp_abc_df_mat),
+                3,
+            )
 
     pairs_artists = generate_pairs_of_artist()
+    for pair in pairs_artists:
+        AGGREGATE_PAIR_ARTISTS[pair] = {}
+        songs1 = get_songs_from_artist(pair[0])
+        songs2 = get_songs_from_artist(pair[1])
+        temp_lyrics_df = cosine_sim_lyrics_df.loc[songs1][songs2]
+        temp_lyrics_df_mat = temp_lyrics_df.to_numpy()
+        # try:
+        #     AGGREGATE_PAIR_ARTISTS[pair]["Lyrics"] = np.linalg.det(temp_lyrics_df_mat)
+        # except:
+        #     print(pair)
+
+        # Uncomment when ABC files are present
+        temp_abc_df = cosine_sim_abc_df.loc[songs1][songs2]
+        temp_abc_df_mat = temp_abc_df.to_numpy()
+        # AGGREGATE_PAIR_ARTISTS[pair]['ABC'] = np.linalg.det(temp_abc_df_mat)
+
+        AGGREGATE_PAIR_ARTISTS[pair] = round(
+            1 - get_absolute_differences_mean(temp_lyrics_df_mat, temp_abc_df_mat), 3
+        )
+
+    pprint(AGGREGATE_ARTIST)
+    pprint(AGGREGATE_PAIR_ARTISTS)
 
 
 if __name__ == "__main__":
